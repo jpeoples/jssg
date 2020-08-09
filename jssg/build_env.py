de42dfa484f934a5553fe009b390c9ff38b8efe6
Jacob Peoples <jacobjpeoples@gmail.com>
2020-08-09 13:04:23 -0400

    Remove unnecessary BuildEnv class

--- jssg/build_env.py
+++ jssg/build_env.py
@@ -1,54 +1,80 @@
 import fnmatch
+import pathlib
+import os
+import shutil
 
-from .execution_rule import FileSys, ExecutionRule
+from .execution_rule import ExecutionRule
 
-def build(indir, outdir, rules, files="", filesys=None, listeners={}):
+
+def build(indir, outdir, rules, files="", filesys=None, listeners=None):
     """Build files in `indir` to `outdir` given a set of `rules`"""
 
-    return BuildEnv(indir, outdir, filesys, listeners).build(rules, files)
+    if filesys is None:
+        filesys=FileSys(indir, outdir)
 
-class BuildEnv:
-    def __init__(self, indir, outdir, filesys=None, listeners={}):
-        self.indir = indir
-        self.outdir = outdir
-        self.fs = filesys if filesys is not None else FileSys(indir, outdir)
-        self.listeners = listeners
+    if isinstance(files, str):
+        files = filesys.list_all_files(files)
 
-    def _translate_to_execution(self, rule, file):
-        if rule is None: return None
-        pm, rule = rule
-        #inf, outf = execute_path_map(pm, file, self.indir, self.outdir)
-        inf = file
-        outf = pm(file)
-        rule = ExecutionRule.wrap(rule)
-        execution, data = rule(self.fs, inf, outf)
-        if data is not None:
-            for l in self.listeners.values():
-                if hasattr(l, "on_data_return"):
-                    l.on_data_return(inf, outf, data)
-        return execution
+    rf_pairs = match_files_to_rules(rules, files)
+    exec_state, executions = evaluate_rules(filesys, rf_pairs, listeners)
 
-    def build(self, rules, files=""):
-        executions = []
-        if isinstance(files, str):
-            files = self.fs.list_all_files(files)
-        for file in files:
-            rule = _first_matching_rule(rules, file)
-            ex = self._translate_to_execution(rule, file)
-            if ex is not None:
-                executions.append(ex)
+    for ex in executions:
+        ex(exec_state)
 
-        self._run_executions(executions)
+# Given matching rules, compute
+# a) A list of (inf, outf, data)
+# b) executions
 
-    def _run_executions(self, executions):
-        exec_state = {}
-        for name, l in self.listeners.items():
-            if hasattr(l, 'before_execute'):
-                exec_state[name] = l.before_execute()
+# Match files to rules does the first step, of selecting appropriate
+# rules for each file
+def match_files_to_rules(rules, files):
+    return ((_first_matching_rule(rules, f), f) for f in files)
 
-        for ex in executions:
-            ex(exec_state)
+def evaluate_rules(fs, rule_file_pairs, listeners=None):
+    dat, executions = zip(*lazy_evaluate_rules(fs, rule_file_pairs))
+    exec_state = {}
+    if listeners:
+        exec_state = notify_listeners(dat, listeners)
+    return exec_state, executions
 
+def lazy_evaluate_rules(fs, rule_file_pairs):
+    for rule, f in rule_file_pairs:
+        outf, (ex, data) = execute_rule(fs, rule, f)
+        yield (f, outf, data), ex
+
+
+
+def translate_to_executions(fs, rules, files):
+    data_returns = []
+    executions = []
+    for rule, f in match_files_to_rules(rules, files):
+        outf, (ex, dat) = execute_rule(fs, rule, f)
+        data_returns.append((f, outf, dat))
+        executions.append(ex)
+    return data_returns, executions
+
+
+def execute_rule(fs, rule, f):
+    if rule is None: return None
+    pm, rule = rule
+    outf = pm(f)
+    rule = ExecutionRule.wrap(rule)
+    return outf, rule(fs, f, outf)
+
+
+def notify_listeners(inputs, listeners):
+    for (inf, outf, dat) in inputs:
+        if dat is not None:
+            for l in listeners.values():
+                if hasattr(l, 'on_data_return'):
+                    l.on_data_return(inf, outf, dat)
+
+    exec_state = {}
+    for name, l in listeners.items():
+        if hasattr(l, 'before_execute'):
+            exec_state[name] = l.before_execute()
+
+    return exec_state
 
 def _first_matching_rule(rules, path, default=None):
     for (rule, result) in rules:
@@ -64,3 +90,43 @@ def _match_single_rule(rule, path):
         if _match_single_rule(r, path):
             return True
     return False
+
+def list_all_files(d, rel_to=None):
+    if rel_to is None:
+        rel_to = d
+
+    for directory_path, _, file_names in os.walk(d):
+        for fn in file_names:
+            yield pathlib.Path(directory_path, fn).relative_to(rel_to).as_posix()
+
+class FileSys:
+    def __init__(self, indir, outdir):
+        self.indir = indir
+        self.outdir = outdir
+
+    def resolve_in(self, inf):
+        return pathlib.Path(self.indir, inf).as_posix()
+
+    def resolve_out(self, outf):
+        return pathlib.Path(self.outdir, outf).as_posix()
+
+
+    def copy(self, inf, outf):
+        self.ensure_dir(outf)
+        shutil.copy(self.resolve_in(inf), self.resolve_out(outf))
+
+    def read(self, inf):
+        return open(self.resolve_in(inf), 'r', encoding='utf-8').read()
+
+    def write(self, outf, s):
+        self.ensure_dir(outf)
+        open(self.resolve_out(outf), 'w', encoding='utf-8').write(s)
+
+    def ensure_dir(self, outf):
+        outf = self.resolve_out(outf)
+        loc = pathlib.Path(outf).parent
+        if not loc.is_dir():
+            loc.mkdir(parents=True)
+
+    def list_all_files(self, d=""):
+        return list_all_files(self.resolve_in(d), rel_to=self.indir)
--- jssg/execution_rule.py
+++ jssg/execution_rule.py
@@ -1,7 +1,3 @@
-import pathlib
-import shutil
-import os
-
 class ExecutionRule:
     @classmethod
     def wrap(cls, cl):
@@ -10,18 +6,6 @@ class ExecutionRule:
             return CallableExecutionRule(cl)
         else:
             return cl
-
-def two_phase_rule(f):
-    class UserExecutionRule(ExecutionRule):
-        # preserve help to some degree
-        __name__ = f.__name__
-        __doc__ = f.__doc__
-        def __init__(self, f):
-            self.f = f
-        def __call__(self, fs, inf, outf):
-            return self.f(fs, inf, outf)
-
-    return UserExecutionRule(f)
 
 def _execute_callable_as_filemap(f, fs, inpath, outpath):
     try:
@@ -48,43 +32,5 @@ class CallableExecutionRule(ExecutionRule):
 def copy_file(fs, inpath, outpath):
     fs.copy(inpath, outpath)
 
-def list_all_files(d, rel_to=None):
-    if rel_to is None:
-        rel_to = d
 
-    for directory_path, _, file_names in os.walk(d):
-        for fn in file_names:
-            yield pathlib.Path(directory_path, fn).relative_to(rel_to).as_posix()
-
-class FileSys:
-    def __init__(self, indir, outdir):
-        self.indir = indir
-        self.outdir = outdir
-
-    def resolve_in(self, inf):
-        return pathlib.Path(self.indir, inf).as_posix()
-
-    def resolve_out(self, outf):
-        return pathlib.Path(self.outdir, outf).as_posix()
-
-
-    def copy(self, inf, outf):
-        self.ensure_dir(outf)
-        shutil.copy(self.resolve_in(inf), self.resolve_out(outf))
-
-    def read(self, inf):
-        return open(self.resolve_in(inf), 'r', encoding='utf-8').read()
-
-    def write(self, outf, s):
-        self.ensure_dir(outf)
-        open(self.resolve_out(outf), 'w', encoding='utf-8').write(s)
-
-    def ensure_dir(self, outf):
-        outf = self.resolve_out(outf)
-        loc = pathlib.Path(outf).parent
-        if not loc.is_dir():
-            loc.mkdir(parents=True)
-
-    def list_all_files(self, d=""):
-        return list_all_files(self.resolve_in(d), rel_to=self.indir)
 
